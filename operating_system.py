e57d6f4fbefc0a59429c95c567babc7145cdb5b4
benraivel <benraivel@gmail.com>
2022-02-16 20:39:12 -0500

    Actually got things mostly working

--- operating_system.py
+++ operating_system.py
@@ -1,5 +1,5 @@
-import process
-import scheduler
+import process as p
+import scheduler as s
 import pandas as pd
 import random
 
@@ -7,19 +7,55 @@ from scipy.stats import dlaplace, boltzmann, planck
 
 
 def kernel(scheduler, verbose = True):
+    '''
+    uses scheduler function to schedule processes and record statistics
+    '''
     CPU = []
     ready = []
-
     time = 0
+    
+    processes = [p.Process(0, 3, 0, 4),
+                 p.Process(1, 6, 1, 7),
+                 p.Process(2, 2, 2, 2),
+                 p.Process(3, 12, 3, 6)]
+
+    s.add_ready(processes, ready, time)
+
+    while len(ready) > 0:
+        time = scheduler(processes, ready, CPU, time, verbose)
+
+    for process in CPU:
+        PID = process[process]
+
+    df = pd.DataFrame(CPU)
+    df.to_csv('results.csv', index=False)
+    
+        
 
 
-# generate 100 random arrival times, evenly distributed
-arrival_times = []
-for i in range(100):
-    arrival_times.append(random.randint(0,100))
-arrival_times.sort()
+def generate_processes(n_process = 100, arrival_freq = 1):
+    # generate n random arrival times, evenly distributed
+    arrival_times = []
+    for i in range(n_process):
+        arrival_times.append(random.randint(0,n_process*arrival_freq))
+    arrival_times.sort()
+    
+    # generate n random burst times following planck distribution
+    burst_times = planck.rvs(0.4, size = n_process) + 1
 
-# generate 100 random burst times following planck distribution
-burst_times = planck.rvs(0.4, size = 100) + 1
+    # construct list of processes
+    processes = []
+    for i in range(n_process):
+        processes.append(process.Process(i, burst_times[i], arrival_times[i], random.randint(0,5)))
 
-print(arrival_times, burst_times, sum(burst_times)/100)
+    return processes
+
+#kernel(s.FCFS_scheduler)
+
+
+print()
+kernel(s.FCFS_scheduler)
+
+kernel(s.SJF_scheduler)
+
+kernel(s.priority_scheduler)
--- scheduler.py
+++ scheduler.py
@@ -2,38 +2,188 @@
 # Ben Raivel
 # process scheduling algorithm implementations
 
+import numpy as np
+import process
+
 def FCFS_scheduler(processes, ready, CPU, time, verbose = True):
     '''
     first come first served scheduling algorithm
     '''
-    pass
 
-def SJF_scheduler():
+    # get process with lowest arrival
+    current_process = find_lowest_arrival(ready)
+
+    # record start time
+    start_time = time
+
+    # while burst time remains
+    while(current_process.get_burst_time() > 0):
+
+        #decrement burst time
+        current_process.set_burst_time(current_process.get_burst_time()-1)
+
+        # increment time
+        time += 1
+
+        add_ready(processes, ready, time)
+
+    end_time = time
+    
+    CPU.append(dict(process = current_process.get_PID(), 
+                    start = start_time,
+                    finish = end_time,
+                    priority = current_process.get_priority()))
+
+    if(verbose):
+        print('__________________\n'+
+            'PID\t\t'+ str(current_process.get_PID()) + '\n'+ 
+            'start time\t' + str(start_time) + '\n' + 
+            'end time\t' + str(end_time) + '\n'+
+            'priority\t' + str(current_process.get_priority()))
+
+    return time
+
+def SJF_scheduler(processes, ready, CPU, time, verbose = True):
     '''
     shortest job first scheduling algorithm
     '''
-    pass
+    current_process = find_shortest(ready)
 
-def priority_scheduler():
+    # record start time
+    start_time = time
+
+    # while burst time remains
+    while(current_process.get_burst_time() > 0):
+
+        #decrement burst time
+        current_process.set_burst_time(current_process.get_burst_time()-1)
+
+        # increment time
+        time += 1
+
+        add_ready(processes, ready, time)
+
+    end_time = time
+    
+    CPU.append(dict(process = current_process.get_PID(), 
+                    start = start_time,
+                    finish = end_time,
+                    priority = current_process.get_priority()))
+
+    if(verbose):
+        print('__________________\n'+
+            'PID\t\t'+ str(current_process.get_PID()) + '\n'+ 
+            'start time\t' + str(start_time) + '\n' + 
+            'end time\t' + str(end_time) + '\n'+
+            'priority\t' + str(current_process.get_priority()))
+
+    return time
+
+def priority_scheduler(processes, ready, CPU, time, verbose = True):
     '''
     priority scheduling algorithm
     '''
-    pass
+    # get process with lowest arrival
+    current_process = find_highest_priority(ready)
+
+    # record start time
+    start_time = time
+
+    # while burst time remains
+    while(current_process.get_burst_time() > 0):
+
+        #decrement burst time
+        current_process.set_burst_time(current_process.get_burst_time()-1)
+
+        # increment time
+        time += 1
+
+        add_ready(processes, ready, time)
+
+    end_time = time
+    
+    CPU.append(dict(process = current_process.get_PID(), 
+                    start = start_time,
+                    finish = end_time,
+                    priority = current_process.get_priority()))
+
+    if(verbose):
+        print('__________________\n'+
+            'PID\t\t'+ str(current_process.get_PID()) + '\n'+ 
+            'start time\t' + str(start_time) + '\n' + 
+            'end time\t' + str(end_time) + '\n'+
+            'priority\t' + str(current_process.get_priority()))
+
+    return time
 
 def find_lowest_arrival(ready):
     '''
     helper function to return earliest-arrived process from the ready queue
     '''
-    lowest_arrival = ready[0]
-    
+    arrival = []
     for process in ready:
-        if process.get_arrival_time() < lowest_arrival.get_arrival_time():
-            lowest_arrival = process
+        arrival.append(process.get_arrival_time())
+    
+    lowest = np.argmin(np.asarray(arrival))
 
-    return lowest_arrival
+    return ready.pop(lowest)
+
+def find_shortest(ready):
+    burst = []
+    for process in ready:
+        burst.append(process.get_burst_time())
+
+    lowest = np.argmin(np.asarray(burst))
+
+    return ready.pop(lowest)
+
+def find_highest_priority(ready):
+    priority = []
+    for process in ready:
+        priority.append(process.get_priority())
+
+    highest = np.argmax(np.asarray(priority))
+
+    return ready.pop(highest)
 
 def add_ready(processes, ready, time):
     '''
     helper function, moves processes into ready queue at correct time
     '''
+    for process in processes:
+        if process.get_arrival_time() == time:
+            ready.append(process)
+            
+
+
+def main():
+    p0 = process.Process(0, 3, 0, 4)
+    p1 = process.Process(1, 6, 3, 7)
+    p2 = process.Process(2, 2, 9, 2)
+    p3 = process.Process(3, 12, 10, 6)
+
+    ready = [p0, p1]
+
+    lowest_arrival = find_lowest_arrival(ready)
+
+    print(lowest_arrival.get_arrival_time())
+
+    ready = [p3, p2, p1]
+
+    lowest_arrival = find_lowest_arrival(ready)
+
+    print(lowest_arrival.get_arrival_time())
+
+    processes = [p0, p1, p2, p3]
+
+    ready = []
+
+    time = 0
+
+    add_ready(processes, ready, time)
+
+    print(ready, processes)
+
+if __name__ == '__main__':
+    #main()
     pass
